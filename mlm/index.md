432d3f7f11609d646cef178f867682367f3c2dd3
Jane Liang <jane.w.liang@gmail.com>
2019-10-15 23:07:10 -0400

    Added MLM page

---
+++ mlm/index.md
@@ -0,0 +1,12 @@
+---
+layout: default
+title: Matrix Linear Models
+---
+
+<h2><a href="{{ page.url }}" style="color:inherit">{{ page.title }}</a></h2>
+
+<h3>Matrix linear models for genetic screens</h3>
+
+<h3>L<sub>1</sub>  penalized sparse matrix linear models</h3>
+
+Page under construction. 
--- non-site/qr.r
+++ non-site/qr.r
@@ -1,11 +1,17 @@
 library(qrencoder)
 
-# Website URL
-url = "https://janewliang.github.io"
 
 # Generate QR code for website
 png("../media/qrcode.png")
 par(mar=c(3,3,3,3))
-image(qrencode_raster(url), 
+image(qrencode_raster("https://janewliang.github.io"), 
+      asp=1, col=c("white", "black"), axes=FALSE, xlab="", ylab="")
+dev.off()
+
+
+# Generate QR code for matrix linear models page
+png("../media/qrcode_mlm.png")
+par(mar=c(3,3,3,3))
+image(qrencode_raster("https://janewliang.github.io/mlm"), 
       asp=1, col=c("white", "black"), axes=FALSE, xlab="", ylab="")
 dev.off()
